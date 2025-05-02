1a7911e044cca186a8b23bac4be6e9fe8551326d
ちりめん <96675462+tirimen-sub@users.noreply.github.com>
2025-05-03 04:12:44 +0900

    bgpath

--- src/bgVideo.js
+++ src/bgVideo.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
-
+import vodouga01 from './vodouga01.mp4';
+import './App.css';  // .bg-video のスタイル定義があるファイルをインポート
 
 const BgVideo = () => (
   <video
@@ -11,7 +11,8 @@ const BgVideo = () => (
     loop
     preload="auto"
   >
-    <source src="./vodouga01.mp4" type="video/mp4" />
+    <source src={vodouga01} type="video/mp4" />
+    {/* 必要なら別形式も import して <source> を追加 */}
   </video>
 );
 

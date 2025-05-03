5c935c1f39c9a6b39e9086c7077fb2597641875d
ちりめん <96675462+tirimen-sub@users.noreply.github.com>
2025-05-03 15:07:27 +0900

    bubble changed

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
         {questions.map(q => (
           <div
             key={q.id}
-            className={`bubble ${q.answered ? 'answered' : ''}`}
+            className= "bubble"
             style={q.style}
             onClick={() => onSelect(q)}
           >
